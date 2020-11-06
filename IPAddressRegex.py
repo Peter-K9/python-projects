596560adf9637c5866f33e05a39d3e53702ad913
CheffGeoff <66649935+CheffGeoff@users.noreply.github.com>
2020-11-06 15:57:27 -0600

    Add files via upload
    This script will parse a server log file and will find the IP of someone that accessed the server and which port they used to access it. Would have to adjust the regex to the correct IP of the server.

---
+++ IPAddressRegex.py
@@ -0,0 +1,23 @@
+# Peter Kalis
+# CSC-382
+# Exam 2
+
+#library that is used for regex
+import re
+
+#output file for the IP's and ports
+results = open("Router_Security_Notifications_Results.txt", "w")
+
+#regex that will find the guest IP and port
+pattern = re.compile("(\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}):(\d*)(\s->\s)98\.234\.56\.78:(\d*)")
+
+#loops through the data file to find the IP's and ports
+for line in open("Router_Security_Notifications.txt"):
+    #searchs the text for any matches to the regex
+    for match in re.finditer(pattern, line):
+        #If a match is found, it is written to the results file
+        results.write(match.group(1) + "," + match.group(4)+"\n")
+        
+
+results.close()
+
